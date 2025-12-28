e38c952903e784b8b40675797239afb86c0f501e
alexdhami <b2ok369@proton.me>
2025-12-28 10:22:35 +0545

    function parameters order change

--- src/pieces_logic/change_position.h
+++ src/pieces_logic/change_position.h
@@ -2,5 +2,5 @@
 #define CHANGE_POS
 #include "headers.h"
 
-void move_piece(std::unordered_map<std::string,std::string> &current_pieces_pos,const std::string &new_pos,const std::string &old_pos);
+void move_piece(std::unordered_map<std::string, std::string> &curr_pieces_pos,const std::string &old_pos, const std::string &new_pos);
 #endif
