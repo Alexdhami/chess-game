45470a75b40499ab5e3e5d038db1b9b41cc5fe02
alexdhami <b2ok369@proton.me>
2025-12-28 10:33:47 +0545

    Add comments and code optimization

--- src/pieces_logic/sq_names.cpp
+++ src/pieces_logic/sq_names.cpp
@@ -1,18 +1,21 @@
 #include "sq_names.h"
+
+// this function gives all 64 square names.
+
 std::vector<std::vector<std::string>> get_square_names(){
     std::vector<std::vector<std::string>> squarenames;
 
-
-    std::vector<std::string> sqN = {"a","b","c","d","e","f","g","h"};
     for (int i{1}; i<9;i++){
         std::vector<std::string> rows;
+
         for (int j{1}; j<9; j++){
-            std::string var = std::format("{}{}",sqN[i-1],j);
+            char cols = 'a' + (j-1); // 'a' + 0 = a | 'a' + 1 = b | ...
+            std::string var = std::format("{}{}",cols,i);
             rows.push_back(var);
+
         }
         squarenames.push_back(rows);
     }
     return squarenames;
-
 
 }
