740032eeec2ae1b12eeed076efe8ab18416ddb0e
alexdhami <b2ok369@proton.me>
2025-12-28 10:13:40 +0545

    pieces_position change define

---
+++ src/pieces_logic/change_position.h
@@ -0,0 +1,6 @@
+#ifndef CHANGE_POS
+#define CHANGE_POS
+#include "headers.h"
+
+void move_piece(std::unordered_map<std::string,std::string> &current_pieces_pos,const std::string &new_pos,const std::string &old_pos);
+#endif
