c21ec37c6e04d5aa52adcb70eead65babd68267d
alexdhami <b2ok369@proton.me>
2025-12-28 10:38:24 +0545

    comment add

--- src/pieces_logic/change_position.cpp
+++ src/pieces_logic/change_position.cpp
@@ -1,5 +1,7 @@
 #include "change_position.h"
 
+// function takes addresses of curr_pieces_pos, old_pos and new_pos. 
+
 void move_piece(std::unordered_map<std::string, std::string> &curr_pieces_pos,const std::string &old_pos, const std::string &new_pos){
     curr_pieces_pos[new_pos] = curr_pieces_pos.at(old_pos);
     curr_pieces_pos.erase(old_pos);
