b28630ed07914760c073381b3832c88b0e395899
alexdhami <b2ok369@proton.me>
2025-12-29 10:38:04 +0545

    typedef add arr_2d_str

--- src/pieces_logic/sq_names.h
+++ src/pieces_logic/sq_names.h
@@ -2,6 +2,8 @@
 #define SQUARES_NAME
 #include "headers.h"
 
-std::vector<std::vector<std::string>> get_square_names();
+typedef std::vector<std::vector<std::string>> arr_2d_str;
+
+ arr_2d_str get_square_names();
 
 #endif
