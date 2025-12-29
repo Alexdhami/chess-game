dc3b1e654d95d51b458c5793df7a330507f5fc89
alexdhami <b2ok369@proton.me>
2025-12-29 10:39:09 +0545

    typedef update and use

--- src/pieces_logic/sq_names.cpp
+++ src/pieces_logic/sq_names.cpp
@@ -1,16 +1,18 @@
 #include "sq_names.h"
 
 // this function gives all 64 square names.
+typedef std::vector<std::string> arr_1d_str ;
+typedef std::string str;
 
-std::vector<std::vector<std::string>> get_square_names(){
-    std::vector<std::vector<std::string>> squarenames;
+arr_2d_str get_square_names(){
+    arr_2d_str squarenames;
 
     for (int i{1}; i<9;i++){
-        std::vector<std::string> rows;
+        arr_1d_str rows;
 
         for (int j{1}; j<9; j++){
             char cols = 'a' + (j-1); // 'a' + 0 = a | 'a' + 1 = b | ...
-            std::string var = std::format("{}{}",cols,i);
+            str var = std::format("{}{}",cols,i);
             rows.push_back(var);
 
         }
