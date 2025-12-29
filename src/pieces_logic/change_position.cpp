7f00fb32fd3879cfa1856f6442bfa581ab3de0a1
alexdhami <b2ok369@proton.me>
2025-12-29 10:43:07 +0545

    typedef use dict, str

--- src/pieces_logic/change_position.cpp
+++ src/pieces_logic/change_position.cpp
@@ -2,7 +2,7 @@
 
 // function takes addresses of curr_pieces_pos, old_pos and new_pos. changes the position from old to new and writes to curr_pieces_pos hashmap.
 
-void move_piece(std::unordered_map<std::string, std::string> &curr_pieces_pos,const std::string &old_pos, const std::string &new_pos){
+void move_piece(dict &curr_pieces_pos,const str &old_pos, const str &new_pos){
     curr_pieces_pos[new_pos] = curr_pieces_pos.at(old_pos);
     curr_pieces_pos.erase(old_pos);
 
